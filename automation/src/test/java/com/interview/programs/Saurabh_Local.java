6bf675b6b568c5bb8195599f5dd5ce52123bf1b0
“Saurabh” <sourabh07patwari@gmail.com>
2018-06-20 23:45:15 -0700

    This is Saurabh Local

---
+++ automation/src/test/java/com/interview/programs/Saurabh_Local.java
@@ -0,0 +1,10 @@
+package com.interview.programs;
+
+public class Saurabh_Local {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This message is from Saurabh");
+	}
+
+}
